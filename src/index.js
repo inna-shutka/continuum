f2d706dc0b34acbffbf9a12c12f6a55f82fbaf96
INNA SHUTKA <inna102992@gmail.com>
2023-03-30 02:58:09 -0500

    add sections

--- src/index.js
+++ src/index.js
@@ -22,3 +22,72 @@ function changeImageTwo() {
     closeButton.style.display = "none";
     menuCard.style.display = "none";
 };
+
+
+const slideOne = document.getElementById("slide1");
+const slideTwo = document.getElementById("slide2");
+const slideThree = document.getElementById("slide3");
+const slideFour = document.getElementById("slide4");
+const slideFive = document.getElementById("slide5");
+const slideSix = document.getElementById("slide6");
+const dotsContainer = document.querySelector(".course__slider");
+
+document.getElementById("dot1").addEventListener("click", () => {
+    slideOne.style.display = "flex";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "none";
+    slideFour.style.display = "none";
+    slideFive.style.display = "none";
+    slideSix.style.display = "none";
+    dotsContainer.className = "course__slider first-current";
+});
+
+document.getElementById("dot2").addEventListener("click", () => {
+    slideOne.style.display = "none";
+    slideTwo.style.display = "flex";
+    slideThree.style.display = "none";
+    slideFour.style.display = "none";
+    slideFive.style.display = "none";
+    slideSix.style.display = "none";
+    dotsContainer.className = "course__slider second-current";
+});
+
+document.getElementById("dot3").addEventListener("click", () => {
+    slideOne.style.display = "none";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "flex";
+    slideFour.style.display = "none";
+    slideFive.style.display = "none";
+    slideSix.style.display = "none";
+    dotsContainer.className = "course__slider third-current";
+});
+
+document.getElementById("dot4").addEventListener("click", () => {
+    slideOne.style.display = "none";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "none";
+    slideFour.style.display = "flex";
+    slideFive.style.display = "none";
+    slideSix.style.display = "none";
+    dotsContainer.className = "course__slider fourth-current";
+});
+
+document.getElementById("dot5").addEventListener("click", () => {
+    slideOne.style.display = "none";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "none";
+    slideFour.style.display = "none";
+    slideFive.style.display = "flex";
+    slideSix.style.display = "none";
+    dotsContainer.className = "course__slider fifth-current";
+});
+
+document.getElementById("dot6").addEventListener("click", () => {
+    slideOne.style.display = "none";
+    slideTwo.style.display = "none";
+    slideThree.style.display = "none";
+    slideFour.style.display = "none";
+    slideFive.style.display = "none";
+    slideSix.style.display = "flex";
+    dotsContainer.className = "course__slider sixth-current";
+});
